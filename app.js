995f12ee158d74f31e513b579f07ba2175de027b
Claudia <claudiaareneee@gmail.com>
2020-01-12 02:25:14 -0600

    courses are specific to user

--- app.js
+++ app.js
@@ -30,45 +30,65 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 // login page
-app.get('/login', function(req, res) {
+app.get('/login', function (req, res) {
     res.render('pages/login');
 });
 
 // index page
-app.get("/", function(req, res) {
-    let ref = firebase.database().ref("/courses/");
+// app.get("/", function(req, res) {
+//     let ref = firebase.database().ref("/courses/");
+//     let promise = ref.once("value").then(snapshot => {
+//         // console.log(snapshot.val());
+//         // return snapshot.val();
+//         console.log(snapshot.val());
+//         res.render("pages/userRedirect", {values: snapshot.val()});
+//     });
+//     // return promise;
+// });
+
+app.get("/:uid", function (req, res) {
+    let ref = firebase.database().ref("/users/" + req.params.uid + '/courses');
+    let coursesDetails = {};
     let promise = ref.once("value").then(snapshot => {
-        // console.log(snapshot.val());
-        // return snapshot.val();
-        console.log(snapshot.val());
-        res.render("pages/index", {values: snapshot.val()});
+        let userCourses = snapshot.val();
+        let ref = firebase.database().ref("/courses/");
+        ref.once('value').then(snapshot => {
+            let allCourses = snapshot.val();
+            for (const key in userCourses) {
+                if (allCourses.hasOwnProperty(key)) {
+                    coursesDetails[key] = allCourses[key];
+                }
+            }
+        }).then(result => {
+            res.render("pages/index", { values: coursesDetails });
+        });
     });
     // return promise;
 });
 
-app.get("/createCourse", function(req, res) {
+app.get("/createCourse", function (req, res) {
     res.render(__dirname + "/views/pages/createCourse.ejs");
 });
 
-app.get("/course/:id", function(req, res) {
+app.get("/course/:id", function (req, res) {
     let ref = firebase.database().ref("/courses/" + req.params.id + "/");
     let promise = ref.once("value").then(snapshot => {
         // console.log(snapshot.val());
         // return snapshot.val();
         console.log(snapshot.val());
-        res.render(__dirname + "/views/pages/courseDetails.ejs", {values: snapshot.val(), id: req.params.id});
+        res.render(__dirname + "/views/pages/courseDetails.ejs", { values: snapshot.val(), id: req.params.id });
     });
 })
 
 // about page
-app.get("/messages", function(req, res) {
+app.get("/messages", function (req, res) {
     res.render("pages/messages");
 });
 
 app.listen(port);
 console.log(port + " is the magic port");
 
-io.on("connection", function(socket) {
+io.on("connection", function (socket) {
     console.log("a user connected");
 
     socket.on("something", () => {

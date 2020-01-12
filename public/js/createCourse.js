1205ab949e817d5a6d56e70d7beb771dd33ebbd7
Spencer Barnes <interstellarjumper@gmail.com>
2020-01-11 18:06:18 -0600

    Add functionality

--- public/js/createCourse.js
+++ public/js/createCourse.js
@@ -2,9 +2,17 @@ function createCourse(){
   let newId = firebase.database().child('courses').push().key;
   let databaseRef = firebase.database().ref('/courses/'+newId);
 
+  let courseCode = document.getElementById("courseCodeInput").value;
+  let courseName = document.getElementById("courseNameInput").value;
+  let instructorName = document.getElementById("instructorNameInput").value;
+
   databaseRef.set({
-    code: ,
-    name: ,
-    teacher: 
+    id: courseCode,
+    name: courseName,
+    teacher: instructorName
   })
+}
+
+function cancel(){
+  window.history.back();
 }

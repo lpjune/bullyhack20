80a8f86d62bd16c2c532a418ac5a4773b78428da
Claudia <claudiaareneee@gmail.com>
2020-01-12 00:12:14 -0600

    coarses fucking loaded. ejs is a piece of shit

--- public/js/index.js
+++ public/js/index.js
@@ -1,2 +1,9 @@
 function displayCourses() {
+
+    for (const key in courses) {
+        if (courses.hasOwnProperty(key)) {
+            const courses = courses[key];
+            
+        }
+    }
 }

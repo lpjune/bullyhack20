7228442b6e630bb1c73243fd4cd8841fd2e3fe66
lpjune <laurenporsini@gmail.com>
2020-01-12 02:47:09 -0600

    delete em

--- public/js/index 2.js
+++
@@ -1,9 +0,0 @@
-function displayCourses() {
-
-    for (const key in courses) {
-        if (courses.hasOwnProperty(key)) {
-            const courses = courses[key];
-            
-        }
-    }
-}
